0e758060b7f7b2a8bf55e47a037d5d7f6cbefa61
Rahul Tiwari <rahultiwari@10-18-95-49.dynapool.wireless.nyu.edu>
2019-03-06 22:26:45 -0500

    Firebase Integ

--- src/screens/Agent/index.js
+++ src/screens/Agent/index.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import Confirmation from '../../components/Confirmation';
 import { Button, List } from 'semantic-ui-react';
+import axios from '../../axios-details'
 
+
+
+//Fetcing Data....via get
 
 class Agent extends Component{
+
+    state = {
+        posts: [],
+    };
+
+    fetchData=()=> {
+        axios.get(`/details.json`)
+        .then(response => {
+            const data = Object.values(response.data);
+            console.log(response.data)
+            this.setState({ posts : data });
+        })
+    };
+
     render(){
         return(
             <div>
                 <h1>Agent Screen</h1>
+                <Button onClick={this.fetchData}>Click Here</Button>
+                <ul><h3>
+                    { this.state.posts.map(person => <li>{person.firstName}</li>)}
+                    { this.state.posts.map(person => <li>{person.phoneNumber}</li>)}
+                    { this.state.posts.map(person => <li>{person.email}</li>)}
+                    { this.state.posts.map(person => <li>{person.insuranceAgency}</li>)}
+                    { this.state.posts.map(person => <li>{person.DORequest}</li>)}
+                    { this.state.posts.map(person => <li>{person.age}</li>)}
+                    </h3></ul>
             </div>
         )
     }
